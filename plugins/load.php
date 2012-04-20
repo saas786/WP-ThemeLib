5c3fa2a24f8d7dbb69e235a7a2cb5af480657768
Matthew Simo <matthew.a.simo@gmail.com>
2012-04-20 12:06:09 -0500

    Added cpt archives in nav menus plugin.

--- plugins/load.php
+++ plugins/load.php
@@ -4,3 +4,8 @@
  * Load the WordPress Creation Kit
  */
 require_once(TEMPLATEPATH.'/lib/plugins/custom-fields-creator/wck-cfc.php');
+
+/**
+ * Load cpt Archives in nave menus
+ */
+require_once(TEMPLATEPATH.'/lib/plugins/cpt-archives-in-nav-menus.php');
