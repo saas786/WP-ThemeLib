a143d72431164737657cf1f103124b4275f00178
Chris Wallace <chris@walmedia.com>
2012-04-21 13:14:46 -0500

    Added setup scripts to bootstrap file.

--- load.php
+++ load.php
@@ -11,3 +11,8 @@ include_once(THEMELIB.'/plugins/load.php');
  * Load analytics
  */
 include_once(THEMELIB.'/analytics/presstrends.php');
+
+/**
+ * Load theme setup basics
+ */
+include_once(THEMELIB.'/setup/load.php');
